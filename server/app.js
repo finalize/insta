d163b564709d18f2f378b083726fd31d7a131f88
安田将吾 <shogo.yasuda@nijibox.co.jp>
2019-06-03 19:09:42 +0900

    Add login API

--- server/app.js
+++ server/app.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/api", indexRouter);
+
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/index.html"));
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/login', function(req, res, next) {
+  const {id, password} = req.body;
+  if(id === 'kk' && password === '123') {
+    res.json({ id: 'bfej7fj4' });
+  }
+
+  res.json({result: false});
 });
 
 module.exports = router;
