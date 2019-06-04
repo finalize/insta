d1df38d0d56db6fce61b0778f80c1e49f8587949
安田将吾 <shogo.yasuda@nijibox.co.jp>
2019-06-04 20:37:40 +0900

    Create image upload API

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api", indexRouter);
+app.use("/photos", usersRouter);
 
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/index.html"));
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const multer = require('multer')
 var router = express.Router();
 
+const storage =  multer.diskStorage({
+  destination: './files',
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+const uploader = multer({ storage })
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/upload', uploader.single('image'), function(req, res, next) {
+
+  const file = req.file
+  const meta = req.body
+  // デッバグのため、アップしたファイルの名前を表示する
+  console.log(file, meta)
+  // アップ完了したら200ステータスを送る
+  res.status(200).json({msg: 'アップロード完了'})
 });
 
 module.exports = router;
